503813000017f2886082f097f5cebfe85a12772d
WinifredO23 <WinifredOnumbu@outlook.com>
2023-09-22 07:37:22 -0700

    chnges

--- 1-builtin_func.c
+++ 1-builtin_func.c
@@ -1,122 +1,97 @@
 #include "shell.h"
 
 /**
- * _myhistory - displays the history list.
- * @info: Structure containing potential arguments.
- * Return: Always 0
+ * _myhistory - Display the command history with line numbers.
+ * @info: Pointer to the info_t structure.
+ *
+ * Return: Always 0.
  */
 int _myhistory(info_t *info)
 {
-	if (info->history == NULL)
-	{
-		_puts("History is empty or uninitialized.\n");
-		return (1);
-	}
-
 	print_list(info->history);
 	return (0);
 }
 
 /**
- * unset_alias - sets alias to string
- * @info: parameter struct
- * @str: the string alias
- * Return: Always 0 on success, 1 on error
+ * unset_alias - Unset an alias.
+ * @info: Pointer to the info_t structure.
+ * @str: The alias string to unset.
+ *
+ * Return: Always 0 on success, 1 on error.
  */
 int unset_alias(info_t *info, char *str)
 {
-	char *p, c;
-	int d;
+	char *equals_sign, temp_char;
+	int ret;
 
-	p = _strchr(str, '=');
-	if (!p)
-	{
-		_puts("Invalid alias format: Missing '=' character.\n");
+	equals_sign = _strchr(str, '=');
+	if (!equals_sign)
 		return (1);
-	}
-	c = *p;
-	*p = 0;
 
-	d = delete_node_at_index(&(info->alias),
-		get_node_index(info->alias, node_starts_with(info->alias, str, -1)));
-	*p = c;
-	if (d != 0)
-	{
-		_puts("Failed to unset alias.\n");
-		return (1);
-	}
+	temp_char = *equals_sign;
+	*equals_sign = '\0';
 
-	return (0);
+	ret = delete_node_at_index(&(info->alias),
+			get_node_index(info->alias, node_starts_with(info->alias, str, -1)));
+
+	*equals_sign = temp_char;
+	return (ret);
 }
 
 /**
- * set_alias - sets alias to string
- * @info: parameter struct
- * @str: the string alias
- * Return: Always 0 on success, 1 on error
+ * set_alias - Set an alias.
+ * @info: Pointer to the info_t structure.
+ * @str: The alias string to set.
+ *
+ * Return: Always 0 on success, 1 on error.
  */
 int set_alias(info_t *info, char *str)
 {
-	char *p;
+	char *equals_sign;
 
-	p = _strchr(str, '=');
-	if (!p)
-	{
-		_puts("Invalid alias format: Missing '=' character.\n");
+	equals_sign = _strchr(str, '=');
+	if (!equals_sign)
 		return (1);
-	}
 
-	if (!*++p)
-	{
+	if (!*++equals_sign)
 		return (unset_alias(info, str));
-	}
 
 	unset_alias(info, str);
-
-	if (add_node_end(&(info->alias), str, 0) == NULL)
-	{
-		_puts("Failed to set alias.\n");
-		return (1);
-	}
-
-	return (0);
+	return (add_node_end(&(info->alias), str, 0) == NULL);
 }
 
 /**
- * print_alias - prints an alias string
- * @node: the alias node
- * Return: Always 0 on success, 1 on error
+ * print_alias - Print an alias.
+ * @node: Pointer to the alias node.
+ *
+ * Return: Always 0 on success, 1 on error.
  */
 int print_alias(list_t *node)
 {
-	char *p;
+	char *equals_sign_position = NULL, *alias_name = NULL;
 
-	if (node == NULL)
+	if (node)
 	{
-		_puts("Alias node is NULL\n");
-		return (1);
-	}
-
-	p = _strchr(node->str, '=');
-	if (p != NULL)
-	{
-		write(1, node->str, p - node->str);
+		equals_sign_position = _strchr(node->str, '=');
+		for (alias_name = node->str; alias_name <= equals_sign_position; alias_name++)
+			_putchar(*alias_name);
 		_putchar('\'');
-		_puts(p + 1);
+		_puts(equals_sign_position + 1);
 		_puts("'\n");
 		return (0);
 	}
 	return (1);
 }
 /**
- * _myalias - mimics the man alias
- * @info: Structure containing potential arguments.
- * Return: Always 0
+ * _myalias - Mimics the alias builtin (man alias).
+ * @info: Pointer to the info_t structure.
+ *
+ * Return: Always 0.
  */
 int _myalias(info_t *info)
 {
-	int i;
-	char *p = NULL;
+	int i = 0;
+	char *equals_sign_position = NULL;
 	list_t *node = NULL;
 
 	if (info->argc == 1)
@@ -124,25 +99,18 @@ int _myalias(info_t *info)
 		node = info->alias;
 		while (node)
 		{
-			if (print_alias(node) != 0)
-			{
-				_puts("Failed to print alias\n");
-			}
+			print_alias(node);
 			node = node->next;
 		}
-
 		return (0);
 	}
 
 	for (i = 1; info->argv[i]; i++)
 	{
-		p = _strchr(info->argv[i], '=');
-		if (p)
+		equals_sign_position = _strchr(info->argv[i], '=');
+		if (equals_sign_position)
 		{
-			if (set_alias(info, info->argv[i]))
-			{
-				_puts("Failed to set alias\n");
-			}
+			set_alias(info, info->argv[i]);
 		}
 		else
 		{
@@ -152,3 +120,4 @@ int _myalias(info_t *info)
 
 	return (0);
 }
+
